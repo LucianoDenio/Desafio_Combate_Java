a53e2dc676b15a41431367b7d111c8ed7fcce671
Luciano Denio <lucianodenio@gmail.com>
2024-10-09 14:21:00 -0300

    melhorando codigo

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -2,58 +2,59 @@ package aplication;
 
 import java.util.Locale;
 import java.util.Scanner;
-import entities.Champion;
 
+import entities.Champion;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner (System.in);
-		Champion firstHero, secondHero;
+		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Digite os dados do primeiro campeão: ");
 		System.out.print("Nome: ");
-		String heroName = sc.nextLine();
+		String name = sc.nextLine();
 		System.out.print("Vida inicial: ");
-		int lifeInitial = sc.nextInt();
-		System.out.print("Ataque: ");
-		int atk = sc.nextInt();
-		System.out.print("Armadura: ");
-		int def = sc.nextInt();
+		int life = sc.nextInt();
+		System.out.print("Attack: ");
+		int attack = sc.nextInt();
+		System.out.print("Armor: ");
+		int armor = sc.nextInt();
+		sc.nextLine();
+		
+		Champion champion1 = new Champion(name, life, attack, armor);
+		
 		System.out.println();
-		
-		firstHero = new Champion(heroName, lifeInitial, atk, def);
-		
 		System.out.println("Digite os dados do segundo campeão: ");
 		System.out.print("Nome: ");
-		sc.nextLine();
-		heroName = sc.nextLine();
+		name = sc.nextLine();
 		System.out.print("Vida inicial: ");
-		lifeInitial = sc.nextInt();
-		System.out.print("Ataque: ");
-		atk = sc.nextInt();
-		System.out.print("Armadura: ");
-		def = sc.nextInt();
+		life = sc.nextInt();
+		System.out.print("Attack: ");
+		attack = sc.nextInt();
+		System.out.print("Armor: ");
+		armor = sc.nextInt();
+		
+		Champion champion2 = new Champion(name, life, attack, armor);
+		
 		System.out.println();
-		
-		secondHero = new Champion(heroName, lifeInitial, atk, def);
-		
-
-		System.out.print("Quantos turnos você Deseja executar ?");
+		System.out.print("Quantos turnos vão ser: ");
 		int turn = sc.nextInt();
-		System.out.println();
 		
-		for(int i = 1; i < turn+1 && (firstHero.getLife() > 0 && secondHero.getLife() > 0) ; i++ ) {
-			firstHero.takeDamage(secondHero);
-			secondHero.takeDamage(firstHero);
+		for (int i=0;i<turn;i++) {
+			champion1.takeDamage(champion2);
+			champion2.takeDamage(champion1);
 			
-			System.out.println("Resultado do turno " + i);
-			System.out.println(firstHero.status()); 
-			System.out.println(secondHero.status());
-			System.out.println();
+			System.out.println("Resultado do turno "+(i+1));
+			System.out.print(champion1);
+			System.out.println(champion2);
 			
+			if((champion1.getLife() <= 0) || champion2.getLife() <=0) {
+				break;
+			}
 		}
+		
 		System.out.println();
 		System.out.println("FIM DO COMBATE");
 		
--- src/entities/Champion.java
+++ src/entities/Champion.java
@@ -5,8 +5,7 @@ public class Champion {
 	private int life;
 	private int attack;
 	private int armor;
-	
-
+		
 	public Champion(String name, int life, int attack, int armor) {
 		this.name = name;
 		this.life = life;
@@ -14,46 +13,50 @@ public class Champion {
 		this.armor = armor;
 	}
 
-
 	public String getName() {
 		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public int getLife() {
 		return life;
 	}
 
-
 	public int getAttack() {
 		return attack;
 	}
 
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
 
 	public int getArmor() {
 		return armor;
 	}
-	
 
+	public void setArmor(int armor) {
+		this.armor = armor;
+	}
+	
 	public void takeDamage(Champion other) {
-		if(this.armor > other.attack) {
-			this.life --;
-		}
-		else this.life -= other.attack - this.armor;	
+	    if (this.armor >= other.attack) {
+	        this.life = Math.max(0, this.life - 1);
+	    } else {
+	        this.life = Math.max(0, this.life - (other.attack - this.armor));
+	    }
 	}
-	public String status() {
-		if(this.life > 0 ) {
-			return name 
-			 +": "
-			 +life
-			 +" de vida";
-		}
-		else return  name 
-				 +": 0"
-				 +" de vida (morreu)";
-		
+
+	
+	public String toString() {
+	    if (life > 0) {
+	        return name + ": " + String.format("%d de vida%n", life);
+	    } else {
+	        return name + ": 0 de vida (morreu)";
+	    }
 	}
-	
-	
-	
-	
+
+
 }
